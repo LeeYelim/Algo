f76cea2079d294bcd3cdd687a8bd1b6116f8433e
LeeYelim <pkmarin15@naver.com>
2017-01-09 20:56:07 +0900

    170109 Graph Basic Exam

---
+++ Graph/algo/graph/basic/BasicExam.java
@@ -0,0 +1,64 @@
+package algo.graph.basic;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BasicExam {
+	/**
+	 * 깊이 우선 탐색
+	 * */
+	static int n,m,v;
+	static boolean [][] matrix;
+	static List<Integer> array;
+	static Queue<Integer> que;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt(); // 정점 갯수
+		m = sc.nextInt(); // 간선 갯수
+		v = sc.nextInt(); // 탐색 시작 정점 번호
+		matrix = new boolean[n][n]; // 인접행렬
+		array = new ArrayList<Integer>();
+		que = new LinkedList<Integer>();
+		
+		for(int i=0; i<m; i++) {
+			System.out.println("간선 입력");
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			matrix[x-1][y-1]=true;
+			matrix[y-1][x-1]=true;
+		}
+		dfs(v-1); // 0으로 시작
+		System.out.println("================================");
+		bfs(v-1); // 0으로 시작
+	}
+
+	private static void dfs(int v) {
+		array.add(v);
+		for(int i=0; i<matrix[v].length; i++) {
+			if(matrix[v][i]==true && !array.contains(i)) {
+				System.out.println((v+1) + "에서 " + (i+1)+ "로 이동");
+				dfs(i);
+			}	
+		}
+	}
+	
+	private static void bfs(int v) {
+		que.add(v);
+		array = new ArrayList<Integer>();
+		array.add(v);
+		while(!que.isEmpty()) {
+			v = que.poll();
+			for(int i=0; i<matrix[v].length; i++) {
+				if(matrix[v][i]==true && !array.contains(i)) {
+					System.out.println((v+1) + "에서 " + (i+1)+ "로 이동");
+					que.add(i);
+					array.add(i);
+				}
+			}
+		}
+	}
+}
