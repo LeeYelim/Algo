39fc20b3d13d62244721683877d18a5b8ed3d13f
LeeYelim <pkmarin15@naver.com>
2017-01-10 16:53:43 +0900

    170110 16:51 PathSearchExamDFS

--- Graph/algo/graph/dfs/PathSearchExam.java
+++ Graph/algo/graph/dfs/PathSearchExam.java
@@ -1,36 +1,27 @@
 package algo.graph.dfs;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class PathSearchExam {
 
-	static int pathMatrix[][];
-	static int n;
-	static int [][] matrix;
-	static List<Integer> array; 
-	
 	public static void main(String[] args) {
 		Scanner sc = new  Scanner(System.in);
-		n = sc.nextInt(); // 정점 갯수
-		matrix = new int[n][n]; // 인접행렬(방향 고려)
-		pathMatrix = new int[n][n];
-		array = new ArrayList<Integer>(); // 지나간 정점
+		int n = sc.nextInt(); // 정점 갯수
+		int [][] matrix = new int[n][n]; // 인접행렬(방향 고려)
+		int pathMatrix[][] = new int[n][n]; // 결과 행렬
+		int [] visitArray; 
 		
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
 				matrix[i][j] = sc.nextInt(); // 0 : 간선X, 1 : 간선O
 			}
 		}
+		sc.close();
 		
-		for(int i=0; i<n; i++) {
-			array.clear();
-			pathSearch(i);
-			for(int j=0; j<array.size(); j++) {
-				int x = array.get(j);
-				pathMatrix[i][x]=1;
-			}
+		for(int i=0; i<n; i++) { // 정점 만큼 실행 
+			visitArray = new int[n]; // 지나간 정점
+			pathSearch(matrix, visitArray, i, true);
+			pathMatrix[i]=visitArray;
 		}
 		
 		for(int i=0; i<n; i++) {
@@ -45,11 +36,11 @@ public class PathSearchExam {
 	 * 가중치 없는 방향 그래프 G가 주어졌을 때, 
 	 * 모든 정점 (i, j)에 대해서, i에서 j로 가는 경로가 있는지 없는지 구하는 프로그램
 	 * */
-	private static void pathSearch(int v) { // 시작 정점
-		for(int i=0; i<n; i++) {
-			if(matrix[v][i]==1 && !array.contains(i)) {
-				array.add(i); // 정점 추가
-				pathSearch(i);
+	private static void pathSearch(int[][] matrix, int [] visitArray, int v, boolean isFirst) { // 시작 정점
+		if(!isFirst) visitArray[v]=1;
+		for(int i=0; i<matrix[v].length; i++) {
+			if(matrix[v][i]==1 && visitArray[i]==0) {
+				pathSearch(matrix, visitArray, i, false);
 			}
 		}
 	}
